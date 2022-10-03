f1da6386a4ac758a12b11ad89c150dbb651f931e
Nolan Stucky <nolan@stucky.net>
2022-10-03 01:08:25 -0700

    fixed compression issue

---
+++ space_to_jump_2d/Assets/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool grounded = true;
+    private Rigidbody2D rb2d;
+    public float jumpPower;
+    public bool powered;
+    private BoxCollider2D boxCollider2D;
+    [SerializeField] private LayerMask platformsLayerMask;
+
+    void Start()
+    {
+        rb2d = rb2d = GetComponent<Rigidbody2D> ();
+        boxCollider2D = transform.GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
+            rb2d.velocity = Vector2.up * jumpPower;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if (powered == false)
+            {
+                Debug.Log(powered);
+                powered = true;
+                Debug.Log(powered);
+                jumpPower = 10f;
+            }
+            else if (powered == true)
+            {
+                powered = false;
+                jumpPower = 5f;
+            }
+        }
+    }
+
+    private bool IsGrounded() {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, 0.1f, platformsLayerMask);
+        return raycastHit2D.collider != null;
+    }
+
+}
